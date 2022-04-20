a2e55f1cbac42a22075f1eb4896d9d897faefcc1
Koen van Leeuwen <koen@logiqs.nl>
2022-04-20 12:41:16 +0200

    Don't fetch only once

--- TelemetryToRaven/Deploy/ebus.sh
+++ TelemetryToRaven/Deploy/ebus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -f http://localhost:8889/data/720/z1RoomTemp?required=true > /dev/null 2>&1
-curl -f http://localhost:8889/data/720/z1ActualRoomTempDesired?required=true > /dev/null 2>&1
-curl -f http://localhost:8889/data/hmu?required=true > /dev/null 2>&1
+curl -f http://localhost:8889/data/720/z1RoomTemp?maxage=50 > /dev/null 2>&1
+curl -f http://localhost:8889/data/720/z1ActualRoomTempDesired?maxage=50 > /dev/null 2>&1
+curl -f http://localhost:8889/data/hmu?maxage=50 > /dev/null 2>&1
 curl -f http://localhost:8889/data 2>\dev\null
