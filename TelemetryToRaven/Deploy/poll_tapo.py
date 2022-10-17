378dd0c03252c45bc767882815c9d10d89b3c51c
Koen van Leeuwen <koen@logiqs.nl>
2022-10-17 21:09:24 +0200

    Change library to one available via pip3

--- TelemetryToRaven/Deploy/poll_tapo.py
+++ TelemetryToRaven/Deploy/poll_tapo.py
@@ -1,6 +1,6 @@
-﻿#!/usr/bin/python3
+#!/usr/bin/python3
 import argparse
-from PyTapo.devices import P110
+from PyP100 import PyP110 # PyP100v2 pip package
 import json
 parser = argparse.ArgumentParser(description='Poll information from a Tapo P110 or compatible plug, output as json')
 parser.add_argument('host', help='the hostname or IP of the plug')
@@ -8,10 +8,12 @@ parser.add_argument('username', help='Tapo username/email address')
 parser.add_argument('password', help='Tapo password')
 args = parser.parse_args()
 
-dev = P110(args.host, args.username, args.password)
+dev = PyP110.P110(args.host, args.username, args.password)
+dev.handshake() #Creates the cookies required for further methods
+dev.login() #Sends credentials to the plug and creates AES Key and IV for further methods
 
-print("{ device_info:")
-print(json.dumps(dev.get_device_info(), sort_keys=True, indent=2))
-print(" energy_usage:")
-print(json.dumps(dev.get_energy_usage(), sort_keys=True, indent=2))
+print("{ \"device_info\":")
+print(json.dumps(dev.getDeviceInfo(), sort_keys=True, indent=2))
+print(", \"energy_usage\":")
+print(json.dumps(dev.getEnergyUsage(), sort_keys=True, indent=2))
 print("}")
