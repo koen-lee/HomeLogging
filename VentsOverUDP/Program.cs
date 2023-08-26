eea63b5e434129ecaf941a9a1d478cc710bcc403
Koen van Leeuwen <koen@logiqs.nl>
2023-08-26 19:25:41 +0200

    Refactor to predefined commands, checksum calculation
    see also https://blaubergventilatoren.de/uploads/download/b73_9_1en_01preview.pdf

--- VentsOverUDP/Program.cs
+++ VentsOverUDP/Program.cs
@@ -4,14 +4,22 @@ using System.Text;
 
 static class Program
 {
-    public static async Task Main(string host, string serial, string password = "1111")
+    static readonly Dictionary<string, string> Commands = new()
+    {
+        { "Temperatures", "01b91f20212233bbba326a" },
+        { "SwitchOn", "030101" },
+        { "SwitchOff", "030100" },
+    };
+
+    public static async Task Main(string host, string serial, string password = "1111", string command = "Temperatures")
     {
         if (host == null || serial == null)
             throw new ArgumentNullException();
         Console.WriteLine("Hello, World!");
 
+        Commands.TryGetValue(command, out var resolvedcommand);
         var udp = new UdpClient(host, 4000);
-        var request = ComposeCommand("fdfd02", serial, password, "01b91f20212233bbba326a9a07");
+        var request = ComposeCommand(Prefix, serial, password, resolvedcommand ?? command);
 
         udp.Send(request.ToArray());
         Console.WriteLine("Sent");
@@ -24,74 +32,26 @@ static class Program
         else
         {
 
-            Console.WriteLine($"<- {result.RemoteEndPoint}");
-            Console.WriteLine($"<- {GetString(result.Buffer)}");
+            Console.WriteLine($"<- from [{result.RemoteEndPoint}]");
             Console.WriteLine($"<- {Reply.ReadFrom(result.Buffer)}");
         }
     }
 
-    record Reply
-    {
-        public string Serial { get; private set; }
-        public string Password { get; private set; }
-        public string PayloadString => GetString(Payload).Replace("-", "").ToLowerInvariant();
-        public byte[] Command { get; private set; }
-        public byte[] Payload { get; private set; }
-
-        public static Reply ReadFrom(ReadOnlySpan<byte> buffer)
-        {
-            int i = 0;
-            Eat(ref buffer, 3, out var command);
-            Eat(ref buffer, 1, out var serialLength);
-            Eat(ref buffer, serialLength[0], out var serialBytes);
-            Eat(ref buffer, 1, out var passwordLength);
-            Eat(ref buffer, passwordLength[0], out var passwordBytes);
-
-            switch (GetString(command.ToArray()))
-            {
-                case TemperatureReply.CommandBytes:
-                    return new TemperatureReply
-                    {
-                        Command = command.ToArray(),
-                        Serial = Encoding.UTF8.GetString(serialBytes),
-                        Password = Encoding.UTF8.GetString(passwordBytes),
-                        Payload = buffer.ToArray(),
-                    };
-                default:
-
-                    return new Reply
-                    {
-                        Command = command.ToArray(),
-                        Serial = Encoding.UTF8.GetString(serialBytes),
-                        Password = Encoding.UTF8.GetString(passwordBytes),
-                        Payload = buffer.ToArray(),
-                    };
-            }
-        }
-    }
-
-    record TemperatureReply : Reply
-    {
-        public const string CommandBytes = "FD-FD-02";
-        public double OutsideTemperature => BitConverter.ToInt16(Payload,9) / 10.0;
-        public double FanOutTemperature => BitConverter.ToInt16(Payload,24) / 10.0;
-    }
-
-    static void Eat<T>(ref ReadOnlySpan<T> buffer, int length, out ReadOnlySpan<T> eaten)
-    {
-        eaten = buffer.Slice(0, length);
-        buffer = buffer[length..];
-    }
+    static readonly string Prefix = "fdfd02";
 
     static byte[] ComposeCommand(string hexprefix, string serial, string password, string hexpostfix)
     {
         var serialBytes = Encoding.UTF8.GetBytes(serial);
         var passwordBytes = Encoding.UTF8.GetBytes(password);
-        return GetBytes(hexprefix)
+        var command = GetBytes(hexprefix)
             .Append((byte)serialBytes.Length).Concat(serialBytes)
             .Append((byte)password.Length).Concat(passwordBytes)
             .Concat(GetBytes(hexpostfix))
             .ToArray();
+        ushort calculatedSum = 0;
+        foreach (var b in command[2..])
+            calculatedSum += b;
+        return command.Concat(BitConverter.GetBytes(calculatedSum)).ToArray();
     }
 
     static byte[] GetBytes(ReadOnlySpan<char> s)
@@ -102,11 +62,5 @@ static class Program
             result[i] = byte.Parse(s.Slice(2 * i, 2), System.Globalization.NumberStyles.AllowHexSpecifier);
         }
         return result;
-    }
-
-
-    static string GetString(byte[] buffer)
-    {
-        return BitConverter.ToString(buffer);
     }
 }
---
+++ VentsOverUDP/Reply.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+record Reply
+{
+    public string Serial { get; private set; }
+    public string Password { get; private set; }
+    public string PayloadString => GetString(Payload).Replace("-", "").ToLowerInvariant();
+    public byte[] Command { get; private set; }
+    public byte[] Payload { get; private set; }
+
+    public static Reply ReadFrom(ReadOnlySpan<byte> buffer)
+    {
+        VerifyChecksum(buffer);
+        Eat(ref buffer, 3, out var command);
+        Eat(ref buffer, 1, out var serialLength);
+        Eat(ref buffer, serialLength[0], out var serialBytes);
+        Eat(ref buffer, 1, out var passwordLength);
+        Eat(ref buffer, passwordLength[0], out var passwordBytes);
+
+        switch (BitConverter.ToUInt16(buffer))
+        {
+            case TemperatureReply.CommandBytes:
+                return new TemperatureReply
+                {
+                    Command = command.ToArray(),
+                    Serial = Encoding.UTF8.GetString(serialBytes),
+                    Password = Encoding.UTF8.GetString(passwordBytes),
+                    Payload = buffer.ToArray(),
+                };
+            case OnOffReply.CommandBytes:
+                return new OnOffReply
+                {
+                    Command = command.ToArray(),
+                    Serial = Encoding.UTF8.GetString(serialBytes),
+                    Password = Encoding.UTF8.GetString(passwordBytes),
+                    Payload = buffer.ToArray(),
+                };
+            default:
+
+                return new Reply
+                {
+                    Command = command.ToArray(),
+                    Serial = Encoding.UTF8.GetString(serialBytes),
+                    Password = Encoding.UTF8.GetString(passwordBytes),
+                    Payload = buffer.ToArray(),
+                };
+        }
+    }
+
+    private static void VerifyChecksum(ReadOnlySpan<byte> buffer)
+    {
+        if (BitConverter.ToUInt16(buffer) != 0xfdfd)
+            throw new InvalidDataException("header mismatch");
+        ushort calculatedSum = 0;
+        foreach (var b in buffer[2..^2])
+            calculatedSum += b;
+
+        var checksumFromReply = BitConverter.ToUInt16(buffer[^2..]);
+        if (checksumFromReply != calculatedSum)
+            throw new InvalidDataException("checksum mismatch");
+    }
+
+    static string GetString(byte[] buffer)
+    {
+        return BitConverter.ToString(buffer);
+    }
+
+    static void Eat<T>(ref ReadOnlySpan<T> buffer, int length, out ReadOnlySpan<T> eaten)
+    {
+        eaten = buffer.Slice(0, length);
+        buffer = buffer[length..];
+    }
+}
+
+
+record OnOffReply : Reply
+{
+    public const ushort CommandBytes = 0x0106;
+}
+
+record TemperatureReply : Reply
+{
+    public const ushort CommandBytes = 0xFE06;
+    public double OutsideTemperature => BitConverter.ToInt16(Payload, 9) / 10.0;
+    public double FanOutTemperature => BitConverter.ToInt16(Payload, 24) / 10.0;
+}
