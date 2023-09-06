66b25d7790eb33ff4ab01cc4bb4f1ea1909fc83c
Koen van Leeuwen <koen@logiqs.nl>
2023-09-06 11:48:44 +0200

    Parse addresses

--- VentsOverUDP/Reply.cs
+++ VentsOverUDP/Reply.cs
@@ -15,6 +15,62 @@ record Reply
 
     private const ushort Packet_Header = 0xfdfd;
 
+    public enum ItemAddress
+    {
+        OnOff = 1, // 0-Off 1-On 2-toggle
+        SpeedMode = 2, // 1-5
+        MaxSpeed = 3, // 3,5
+        Boost = 6,// 0-Off 1-On 2-toggle
+        Timer = 7,// 0-Off 1-On 2-toggle
+        TimerSpeed = 8, //0-5
+        TimerSetpointMinutes = 9,
+        TimerSetpointHours = 10,
+        TimerCountdown = 11,
+        TimerTemperatureSetpoint = 13,
+        BoostSwitchEnabled = 20,
+        FireAlarmEnabled = 21,
+        RoomTemperatureSetpoint = 24,
+        RoomTemperatureSensorSelection = 29,
+        RoomTemperatureActual = 30,
+        TemperatureOutsideIntake = 31,
+        TemperatureInsideExhaust = 32,
+        TemperatureInsideIntake = 33,
+        TemperatureOutsideExhaust = 34,
+        BoostSwitchStatus = 50,
+        FireAlarmStatus = 51,
+        MinimumFanSpeedSupply = 54,
+        MinimumFanSpeedExtract = 55,
+        SupplySpeed1 = 58,
+        ExtractSpeed1 = 59,
+        SupplySpeed2 = 60,
+        ExtractSpeed2 = 61,
+        SupplySpeed3 = 62,
+        ExtractSpeed3 = 63,
+        SupplySpeed4 = 64,
+        ExtractSpeed4 = 65,
+        SupplySpeed5 = 66,
+        ExtractSpeed5 = 67,
+        DefrostSpeed = 69,
+
+        SupplySpeedBoost = 70,
+        ExtractSpeedBoost = 71,
+        HeaterEnabled = 96,
+        FilterTimerDays = 99,
+        FilterCountdown = 100,
+        ResetFilterCountdown = 101,
+        BoostTurnOnDelay = 102,
+        BoostTurnOffDelay = 103,
+        TemperatureControlEnabled = 104,
+        TemperatureTE5 = 106,
+        RTCTime = 111,
+        RTCDate = 112,
+        WeeklyScheduleEnabled = 114,
+        WeeklyScheduleSpeed = 115,
+        WeeklyScheduleTemperatureSetpoint = 116,
+        ScheduleSetup = 119,
+        MotorHours = 126,
+    }
+
     public static void ReadFrom(ReadOnlySpan<byte> buffer)
     {
         VerifyChecksum(buffer);
@@ -26,10 +82,15 @@ record Reply
         var serialBytes = Eat(ref buffer, serialLength);
         var passwordLength = Eat(ref buffer);
         var passwordBytes = Eat(ref buffer, passwordLength);
-        byte page = 0;
-        byte size = 1;
         var function = Eat(ref buffer);
         if (function != 0x06) throw new InvalidDataException($"Reply function mismatch: got {function}");
+        ParseItems(buffer);
+    }
+
+    private static void ParseItems(ReadOnlySpan<byte> buffer)
+    {
+        byte page = 0;
+        byte size = 1;
         while (buffer.Length > 2)
         {
             var next = Eat(ref buffer);
@@ -47,7 +108,7 @@ record Reply
                 default:
                     var address = page << 8 | next;
                     var data = Eat(ref buffer, size);
-                    Console.WriteLine($"{address:x4} ({address:0000}) = {BitConverter.ToString(data.ToArray())}");
+                    Console.WriteLine($"{(ItemAddress)address} ({address:0000}) = {BitConverter.ToString(data.ToArray())}");
                     size = 1;
                     break;
             }
