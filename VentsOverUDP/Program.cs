175621e6b801e47a1bdd87fab1a6da7fe2556a38
Koen van Leeuwen <koen@logiqs.nl>
2023-11-28 14:56:16 +0100

    Default to nop

--- VentsOverUDP/Program.cs
+++ VentsOverUDP/Program.cs
@@ -1,9 +1,20 @@
 ﻿static class Program
 {
+    /// <summary>
+    /// Communicates with Blauberg/Vents/... decentralized heat recovery fans
+    /// </summary>
+    /// <param name="serial">Fan serial number, as found on the sticker</param>
+    /// <param name="password"></param>
+    /// <param name="host">IP address or hostname, default is broadcast</param>
+    /// <param name="readItems">;-separated list of items to read</param>
+    /// <param name="writeItems">;-separated list of name=hexvalue pairs to write (eg OnOff=01) </param>
+    /// <param name="listItems">Print the known items that may be read or sometimes written</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
     public static async Task Main(string serial,
         string password = "1111",
         string host = "255.255.255.255",
-        string readItems = "TemperatureOutsideIntake;TemperatureOutsideExhaust",
+        string readItems = "",
         string writeItems = "",
         bool listItems = false)
     {
@@ -34,6 +45,7 @@
 
     private static async Task DoRead(string readItems, Device device)
     {
+        if (string.IsNullOrWhiteSpace(readItems)) return;
         var addresses = readItems.Split(';').Select(name => Enum.Parse<ItemAddress>(name)).ToArray();
         var items = await device.ReadAddresses(addresses);
         foreach (var i in items)
