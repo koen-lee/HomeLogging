7f4a2e262d8daf5dd7964171d8668d5c74487499
Koen van Leeuwen <koen@logiqs.nl>
2023-12-24 17:22:03 +0100

    This might work

---
+++ TinkerboardGpioTester/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Device.Gpio;
+
+Console.WriteLine("Hello, World!");
+
+var pin = 184; //gpio_6a0
+using var controller = new GpioController();
+controller.SetPinMode(pin, PinMode.InputPullUp);
+controller.OpenPin(pin, PinMode.Output);
+
+WaitForEventResult result;
+while (true)
+{
+    do
+    {
+        Console.Write(".");
+        result = controller.WaitForEvent(pin, PinEventTypes.Rising, TimeSpan.FromMinutes(5));
+    } while (result.TimedOut);
+    Console.WriteLine("Rise");
+    do
+    {
+        Console.Write(".");
+        result = controller.WaitForEvent(pin, PinEventTypes.Falling, TimeSpan.FromMinutes(5));
+    } while (result.TimedOut); 
+    Console.WriteLine("Fall");
+}
