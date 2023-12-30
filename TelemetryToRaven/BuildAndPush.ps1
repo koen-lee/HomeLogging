1dfebc40b10e16c2fd021852712933f3304fbbd3
Koen van Leeuwen <koen@logiqs.nl>
2023-12-30 16:39:10 +0100

    Update BuildAndPush.ps1

--- TelemetryToRaven/BuildAndPush.ps1
+++ TelemetryToRaven/BuildAndPush.ps1
@@ -1,11 +1,11 @@
 ﻿param( $target = "raspberrypi", $user = "kenw")
 dotnet tool install --global dotnet-deb | write-output # this will only succeed the first time, do not show red warning as it is harmless.
 dotnet deb -r linux-arm | tee output.txt
-$match = select-string -path output.txt -Pattern ".+package '([^']+)' .+";
-if( -not $match.Success ) {
+$match = select-string -path output.txt -Pattern "Created .+ package '([^']+)' .+";
+if( -not $match.Matches.Success ) {
 	throw "Build probably failed"
 }
-$deb = $match.Matches[1].Groups[1].Value;
+$deb = $match.Matches[0].Groups[1].Value;
 $file = (Get-Item $deb).Name
 scp "$deb" ${user}@${target}:/home/kenw/${file}
 ssh ${user}@${target} sudo dpkg -i ${file}
