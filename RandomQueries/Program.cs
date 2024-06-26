79a616653b07876527a8dab2cb1391712eff3764
Koen van Leeuwen <koen@logiqs.nl>
2024-06-26 14:32:40 +0200

    Wip for homewizard battery sanity check

--- RandomQueries/Program.cs
+++ RandomQueries/Program.cs
@@ -7,71 +7,76 @@ using System.Runtime.CompilerServices;
 using TelemetryToRaven;
 using TelemetryToRaven.Gpio;
 
-Console.WriteLine("Hello, World!");
-
-var store = new DocumentStore()
+internal class Program
 {
-    Database = "Eiland17Logging",
-    Urls = new[] { "http://tinkerboard:8080" }
-};
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-store.Conventions.FindCollectionName = type =>
-{
-    if (typeof(Meter).IsAssignableFrom(type))
-        return "Meters";
+        var store = new DocumentStore()
+        {
+            Database = "Eiland17Logging",
+            Urls = new[] { "http://tinkerboard:8080" }
+        };
 
-    return DocumentConventions.DefaultGetCollectionName(type);
-};
-store.Initialize();
-var session = store.OpenSession();
+        store.Conventions.FindCollectionName = type =>
+        {
+            if (typeof(Meter).IsAssignableFrom(type))
+                return "Meters";
+
+            return DocumentConventions.DefaultGetCollectionName(type);
+        };
+        store.Initialize();
+        var session = store.OpenSession();
 
 
-var prices = GetTs(session, "APX", "Apx", "Documents");
-var powers = GetTs(session, "meters/ISK5\\2M550T-1013", "Power");
-var solar1 = GetTs(session, "Meters/SolarRoofFront", "Power");
-var solar2 = GetTs(session, "meters/98D863613CD2", "Power");
+        var prices = GetTs(session, "APX", "Apx", "Documents");
+        var powers = GetTs(session, "meters/ISK5\\2M550T-1013", "Power");
+        var solar1 = GetTs(session, "Meters/SolarRoofFront", "Power");
+        var solar2 = GetTs(session, "meters/98D863613CD2", "Power");
 
-List<Item> items = new();
-foreach (var pr in powers)
-{
-    prices.TryGetValue(pr.Key, out var price);
-    solar1.TryGetValue(pr.Key, out var pv1); /* modbus kWh meter: negative for solar yield, positive for inverter vampire power*/
-    solar2.TryGetValue(pr.Key, out var pv2); /* self reported by inverter: positive for solar yield, no vampire power reported.*/
-    items.Add(new Item(pr.Key, pr.Value / 1000, (pr.Value - pv1 + pv2) / 1000, price));
+        List<Item> items = new();
+        foreach (var pr in powers)
+        {
+            prices.TryGetValue(pr.Key, out var price);
+            solar1.TryGetValue(pr.Key, out var pv1); /* modbus kWh meter: negative for solar yield, positive for inverter vampire power*/
+            solar2.TryGetValue(pr.Key, out var pv2); /* self reported by inverter: positive for solar yield, no vampire power reported.*/
+            items.Add(new Item(pr.Key, pr.Value / 1000, (pr.Value - pv1 + pv2) / 1000, price));
+        }
+
+        Console.WriteLine($"Total {items.Count} items ({100.0 * items.Count / prices.Count:0.00}%)");
+        var consumption = items.Sum(i => i.usage);
+        Print(consumption, "kWh");
+        var consumptionWithoutSolar = items.Sum(i => i.usageWithoutSolar);
+        Print(consumptionWithoutSolar, "kWh");
+        var costWithSolar = items.Sum(i => i.usage * i.cost);
+        Print(costWithSolar, "euro");
+        var costWithoutSolar = items.Sum(i => i.usageWithoutSolar * i.cost);
+        Print(costWithoutSolar, "euro");
+
+        Print(costWithSolar / consumption, "euro/kWh");
+        Print(costWithoutSolar / consumptionWithoutSolar, "euro/kWh");
+
+
+        static void Print(object value, string unit, [CallerArgumentExpression(nameof(value))] string name = default)
+        {
+            Console.WriteLine($"{name}: {value:0.0000} {unit}");
+        }
+
+        static IDictionary<DateTime, double> GetTs(Raven.Client.Documents.Session.IDocumentSession session, string documentId, string tsName, string? collection = default)
+        {
+            var from = new DateTime(2023, 1, 1).ToUniversalTime();
+            var to = from.AddYears(1);
+            Console.WriteLine($"Fetching {documentId}");
+            var result = session.Query<Meter>(collectionName: collection)
+                .Where(c => c.Id == documentId)
+                .Select(q => RavenQuery.TimeSeries(q, tsName, from, to).GroupBy(x => x.Hours(1)).Select(x => x.Average()).ToList())
+                .Single().Results
+                .ToDictionary(x => x.From, x => x.Average[0]);
+            Console.WriteLine(result.Count);
+            return result;
+        }
+    }
 }
-
-Console.WriteLine($"Total {items.Count} items ({100.0 * items.Count / prices.Count:0.00}%)");
-var consumption = items.Sum(i => i.usage);
-Print(consumption, "kWh");
-var consumptionWithoutSolar = items.Sum(i => i.usageWithoutSolar);
-Print(consumptionWithoutSolar, "kWh");
-var costWithSolar = items.Sum(i => i.usage * i.cost);
-Print(costWithSolar, "euro");
-var costWithoutSolar = items.Sum(i => i.usageWithoutSolar * i.cost);
-Print(costWithoutSolar, "euro");
-
-Print(costWithSolar / consumption, "euro/kWh");
-Print(costWithoutSolar / consumptionWithoutSolar, "euro/kWh");
-
-
-static void Print(object value, string unit, [CallerArgumentExpression(nameof(value))] string name = default)
-{
-    Console.WriteLine($"{name}: {value:0.0000} {unit}");
-}
-
-static IDictionary<DateTime, double> GetTs(Raven.Client.Documents.Session.IDocumentSession session, string documentId, string tsName, string? collection = default)
-{
-    var from = new DateTime(2023, 1, 1).ToUniversalTime();
-    var to = from.AddYears(1);
-    Console.WriteLine($"Fetching {documentId}");
-    var result = session.Query<Meter>(collectionName: collection)
-        .Where(c => c.Id == documentId)
-        .Select(q => RavenQuery.TimeSeries(q, tsName, from, to).GroupBy(x => x.Hours(1)).Select(x => x.Average()).ToList())
-        .Single().Results
-        .ToDictionary(x => x.From, x => x.Average[0]);
-    Console.WriteLine(result.Count);
-    return result;
-}
-
 
 record Item(DateTime timestamp, double usage, double usageWithoutSolar, double cost);
---
+++ RandomQueries/ZeroExport.cs
@@ -0,0 +1,117 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Queries;
+using Raven.Client.Documents.Session.TimeSeries;
+using System.Runtime.CompilerServices;
+using TelemetryToRaven;
+using static Raven.Client.Constants;
+
+public class ZeroExport
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello " + nameof(ZeroExport));
+
+        var store = new DocumentStore()
+        {
+            Database = "Eiland17Logging",
+            Urls = new[] { "http://tinkerboard:8080" }
+        };
+
+        store.Conventions.FindCollectionName = type =>
+        {
+            if (typeof(Meter).IsAssignableFrom(type))
+                return "Meters";
+
+            return DocumentConventions.DefaultGetCollectionName(type);
+        };
+        store.Initialize();
+        var session = store.OpenSession();
+
+
+        var powers = GetTs(session, "meters/ISK5\\2M550T-1013", "Power");
+        double BatteryCapacity = 2.700; /*kWh*/
+        double eff = 0.9;
+        double maxPower = 0.800;
+        double maxEnergy = maxPower / 60; // datapoint per minute
+        List<Item> items = new();
+        BatteryState state = new BatteryState(DateTime.MinValue, BatteryCapacity);
+        BatteryState lastState = state;
+        foreach (var pr in powers)
+        {
+            if (pr.Key.Date != state.Timestamp)
+            {
+                Console.WriteLine($"{state.Timestamp}\t{state.Cycles - lastState.Cycles}");
+                lastState = state;
+                state = state with { Timestamp = pr.Key.Date };
+            }
+            var gridEnergy = pr.Value / 1000 / 60; /*wattminute to kwatthour*/
+            if (gridEnergy > 0) /*try discharge*/
+            {
+                var discharge = Min(maxEnergy / eff, gridEnergy / eff, state.SoC);
+                state = state with
+                {
+                    totalDischarged = state.totalDischarged + discharge,
+                    SoC = state.SoC - discharge
+                };
+            }
+            else /*try charge*/
+            {
+                var charge = Min(maxEnergy * eff, -gridEnergy * eff, BatteryCapacity - state.SoC);
+
+                state = state with
+                {
+                    totalCharged = state.totalCharged + charge,
+                    SoC = state.SoC + charge
+                };
+            }
+        }
+
+        Print(state.totalCharged, "kWh");
+        Print(state.totalDischarged, "kWh");
+        Print(state.Cycles, "");
+
+        static void Print(object value, string unit, [CallerArgumentExpression(nameof(value))] string name = default)
+        {
+            Console.WriteLine($"{name}: {value:0.0000} {unit}");
+        }
+
+        static IEnumerable<KeyValuePair<DateTime, double>> GetTs(Raven.Client.Documents.Session.IDocumentSession session, string documentId, string tsName, string? collection = default)
+        {
+            var from = new DateTime(2023, 6, 1).ToUniversalTime().AddHours(2);
+            var to = from.AddYears(1);
+            Console.WriteLine($"Fetching {documentId}");
+
+            var ts = session.TimeSeriesFor(documentId, tsName);
+            List<TimeSeriesEntry> groupedByMinute = new();
+            var minuteTicks = TimeSpan.FromMinutes(1).Ticks;
+            DateTime lastMinute = DateTime.MinValue;
+            using (var s = ts.Stream(from, to))
+            {
+                while (s.MoveNext())
+                {
+                    var thisMinute = new DateTime(s.Current.Timestamp.Ticks / minuteTicks * minuteTicks, DateTimeKind.Utc);
+                    if (thisMinute != lastMinute)
+                    {
+                        if (groupedByMinute.Count > 0)
+                            yield return new KeyValuePair<DateTime, double>(lastMinute, groupedByMinute.Average(x => x.Value));
+                        groupedByMinute.Clear();
+                        lastMinute = thisMinute;
+                    }
+                    groupedByMinute.Add(s.Current);
+                }
+            }
+        }
+
+        static double Min(params double[] doubles)
+        {
+            return doubles.Min();
+        }
+    }
+
+    public record BatteryState(DateTime Timestamp, double batteryCapacity, double SoC = 0, double totalCharged = 0, double totalDischarged = 0)
+    {
+        public double Cycles => (totalCharged + totalDischarged) / 2 / batteryCapacity;
+    }
+}
