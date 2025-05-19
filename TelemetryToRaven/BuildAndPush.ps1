a49d3b2dfb7fbf60fdc85ac327fde2e8af7765a7
Koen van Leeuwen <koen@logiqs.nl>
2025-05-19 15:47:52 +0200

    version bump

--- TelemetryToRaven/BuildAndPush.ps1
+++ TelemetryToRaven/BuildAndPush.ps1
@@ -1,4 +1,6 @@
 ﻿param( $target = "raspberrypi", $user = "kenw")
+
+rm output.txt
 dotnet tool install --global dotnet-deb | write-output # this will only succeed the first time, do not show red warning as it is harmless.
 dotnet deb -r linux-arm | tee output.txt
 $match = select-string -path output.txt -Pattern "Created .+ package '([^']+)' .+";
@@ -7,6 +9,7 @@ if( -not $match.Matches.Success ) {
 }
 $deb = $match.Matches[0].Groups[1].Value;
 $file = (Get-Item $deb).Name
-scp "$deb" ${user}@${target}:/home/kenw/${file}
+do {
+  scp "$deb" ${user}@${target}:/home/kenw/${file}
+} while ($LASTEXITCODE -ne 0 )
 ssh ${user}@${target} sudo dpkg -i ${file}
-rm output.txt
